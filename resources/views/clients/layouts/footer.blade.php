1051c6b0c9a2af77753aa0600faf18c543d99b5e
dainq <97323194+dainqd@users.noreply.github.com>
2023-08-20 01:43:04 +0700

    Home page.

---
+++ resources/views/clients/layouts/footer.blade.php
@@ -0,0 +1,97 @@
+<footer class="footer-sidebar" role="contentinfo">
+    <div class="background-overlay"
+         style="background-image:url({{asset('assets/client/wp-content/uploads/2022/09/bg_09.jpg')}})">
+        <div class="container">
+            <div class="footer-top">
+                <a class="logo-top-footer">
+                    <img src="{{asset('assets/client/wp-content/uploads/2022/09/logo.png')}}" width="156"
+                         height="111" alt=""
+                         decoding="async" loading="lazy"/>
+                </a>
+                <p class="hotline-footer">
+                    <a class="hf-phone" href="tel:1900234538">Hotline: 1900234538 </a>
+                    <a class="hf-phone" href="tel:0961812345">CSKH: 0961812345</a>
+                </p>
+            </div>
+            <div class="footer-bottom">
+                <p class="notice-cn">
+                    <a href="/danh-muc-chi-nhanh/chi-nhanh/" style="color:#ffffff;">Hệ Thống Chi Nhánh</a>
+                </p>
+                <div class="row">
+                    <div class="col-xs-12 col-sm-3 col-md-3 col-lg-3">
+                        <h3 class="cn-h3">Cơ sở 1 </h3>
+                        <p class="cn-p">
+                            <a href="" style="color:#ffffff;">Ngõ 11 Ngọc Khánh, Ba Đình, Hà Nội </a>
+                        </p>
+                    </div>
+                    <div class="col-xs-12 col-sm-3 col-md-3 col-lg-3">
+                        <h3 class="cn-h3">Cơ sở 2 </h3>
+                        <p class="cn-p">
+                            <a href="" style="color:#ffffff;">Số 18 ngõ 75 Trần Quang Diệu, Đống Đa, Hà
+                                Nội</a>
+                        </p>
+                    </div>
+                    <div class="col-xs-12 col-sm-3 col-md-3 col-lg-3">
+                        <h3 class="cn-h3">Cơ sở 3 </h3>
+                        <p class="cn-p">
+                            <a href="" style="color:#ffffff;">Số 76 Ngọc Khánh, Ba Đình, Hà Nội </a>
+                        </p>
+                    </div>
+                    <div class="col-xs-12 col-sm-3 col-md-3 col-lg-3">
+                        <h3 class="cn-h3">Cơ sở 118 </h3>
+                        <p class="cn-p">
+                            <a href="" style="color:#ffffff;">Ngõ 118 Nguyễn Khánh Toàn, Cầu Giấy, Hà
+                                Nội</a>
+                        </p>
+                    </div>
+                    <div class="col-xs-12 col-sm-3 col-md-3 col-lg-3">
+                        <h3 class="cn-h3">Cơ sở 5 </h3>
+                        <p class="cn-p">
+                            <a href="" style="color:#ffffff;">31 Láng Hạ, Ba Đình, Hà Nội</a>
+                        </p>
+                    </div>
+                    <div class="col-xs-12 col-sm-3 col-md-3 col-lg-3">
+                        <h3 class="cn-h3">Cơ sở 6 (NEW) </h3>
+                        <p class="cn-p">
+                            <a href="" style="color:#ffffff;">35B Lê Văn Thiêm, Thanh Xuân Trung, Thanh
+                                Xuân, Hà Nội</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="footer-menu">
+                <ul id="menu-menu-chinh-1" class="">
+                    <li
+                            class="menu-item menu-item-type-post_type menu-item-object-page menu-item-home current-menu-item page_item page-item-13 current_page_item menu-item-499 active">
+                        <a href="https://quannho.vn/">Trang chủ</a>
+                    </li>
+                    <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-494">
+                        <a href="https://quannho.vn/gioi-thieu/">Về chúng tôi</a>
+                    </li>
+                    <li
+                            class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-839 dropdown">
+                        <a href="#">
+                            Menu <i class="caret"></i>
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li
+                                    class="menu-item menu-item-type-post_type menu-item-object-page menu-item-458">
+                                <a href="https://quannho.vn/menu/">Menu</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-453">
+                        <a href="https://quannho.vn/category/news-events/">News &amp;Events</a>
+                    </li>
+                    <li class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-454">
+                        <a href="https://quannho.vn/category/tuyen-dung/">Tuyển dụng</a>
+                    </li>
+                    <li
+                            class="menu-item menu-item-type-taxonomy menu-item-object-type_schools menu-item-496">
+                        <a href="https://quannho.vn/danh-muc-chi-nhanh/chi-nhanh/">Chi nhánh</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
